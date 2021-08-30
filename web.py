e30a586091e90bd5565b5b537d1ccad8db9df89c
Liron Hamdani <lironhamdani2000@gmail.com>
2021-08-30 19:46:59 +0000

    fix env code and ignore env file 3

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import s3
 from flask import Flask, render_template, redirect, request, jsonify
 import s3
 import os
-from dotenv import load_dotnev
+from dotenv import load_dotenv
 load_dotenv()
 key_env  =os.getenv('SECRET_ACCESS_KEY')
 id_env  =os.getenv('_ACCESS_KEY_ID')
