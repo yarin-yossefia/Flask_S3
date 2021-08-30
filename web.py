a92c9bb583a126f5e3dce3858e01c8ef811c5382
Liron Hamdani <lironhamdani2000@gmail.com>
2021-08-30 19:43:36 +0000

    fix env code and ignore env file

--- web.py
+++ web.py
@@ -1,7 +1,11 @@
 import s3
 from flask import Flask, render_template, redirect, request, jsonify
 import s3
-
+import os
+from dotnev import load_dotnev
+load_dotnev()
+key_env  =os.getenv('SECRET_ACCESS_KEY')
+id_env  =os.getenv('_ACCESS_KEY_ID')
 app = Flask(__name__)
 @app.route('/file')
 def GetFile():
@@ -15,6 +19,6 @@ def download():
     id1 = request.form["Id"].strip()
     key1 = request.form["Key"].strip()
     file1 = request.form["File"].strip()
-    s3.Download_file(id1, key1, file1)
+    s3.Download_file(id1 = id_env, key1=key_env, file1=file1)
     return redirect("/file")
 app.run(host='0.0.0.0', port=80, debug=True)
